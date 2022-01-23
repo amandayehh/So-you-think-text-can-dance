547d74210a40ec8c3369926734cc2be7805b8085
amandayehh <amandayeh123@gmail.com>
2022-01-22 23:40:18 -0800

    syncing FFT with variable

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 var fft, amplitude, peakDetect, spectrum, level, treble, highMid, mid, lowMid, bass, centroid;
 var pBass, pLowMid, pMid, pHighMid, pTreble;
 var casl, size;
-var chars;
+var chars, FFTchunks;
 
 function preload() {
-  song = loadSound('doubt.mp3');
+  song = loadSound('iFeel.mp3');
 
   variable = select('.variable');
   playButton = select('.play_button')
@@ -26,15 +26,18 @@ function setup() {
   pHighMid = 0;
   pTreble = 0;
 
+
   //chars = split(variable, '');
   chars = variable.html().split('')
   //console.log(variable.html())
-  console.log(chars);
-  let span = createSpan(chars[0]);
-  span.parent(text)
+  //console.log(chars);
+  // let span = createSpan(chars[0]);
   for (let i = 0; i < chars.length; i++) {
-
+    let span = createSpan(chars[i]);
+    span.parent(text);
   }
+  //console.log(text.child()[0].style.);
+  // console.log(text.child())
 }
 
 function draw() {
@@ -66,25 +69,46 @@ function draw() {
 
 
   // }
-  let h = map(spectrum[500], 0, 255, -800, 1000);
+
   //console.log(h)
   // variable.style('font-variation-settings', " 'SCAN' " + 100);
 
 
-  let waveform = fft.waveform();
-  noFill();
-  beginShape();
-  stroke(20);
-  for (let i = 0; i < waveform.length; i++) {
-    let x = map(i, 0, waveform.length, 0, width);
-    let y = map(waveform[i], -1, 1, 0, height);
-    vertex(x, y);
-    variable.style('font-variation-settings', " 'BLED' " + y + ", 'SCAN' " + h);
-  }
-  endShape();
+  // let waveform = fft.waveform();
+  // noFill();
+  // beginShape();
+  // stroke(20);
+  // for (let i = 0; i < waveform.length; i++) {
+  //   let x = map(i, 0, waveform.length, 0, width);
+  //   let y = map(waveform[i], -1, 1, 0, height);
+  //   vertex(x, y);
+  //   variable.style('font-variation-settings', " 'BLED' " + y + ", 'SCAN' " + h);
+  // }
+  // endShape();
 
   //variable.style('font-weight', '100');
-  casl = map(level, 0, 0.4, 0, 1);
+
+  FFTchunks = splitIntoN(spectrum, text.child().length)
+  //console.log(FFTchunks)
+
+
+
+  //console.log(spectrum)
+  //console.log(getAvg(FFTchunks[0]));
+  let FFTavg = [];
+  for (let i = 0; i < FFTchunks.length; i++) {
+    FFTavg.push(calculateAvg(FFTchunks[i]));
+  }
+
+  for (let i = 0; i < text.child().length; i++) {
+    let wdth = map(FFTavg[i], 0, 255, 50, 200);
+    let size = map(FFTavg[i], 0, 255, 100, 700);
+    text.child()[i].style.fontVariationSettings = "'wdth' " + wdth;
+    //  text.child()[i].style.fontSize = size;
+  }
+
+  //console.log(FFTavg)
+  //console.log(text.child().length)
   //casl = 0.5
   // for (let i = 0; i < variable.length; i++) {
   //   console.log(variable[i]);
@@ -97,6 +121,7 @@ function draw() {
 
   //variable[1].style('font-size', pSize - 10);
 
+
 }
 
 function togglePlaying() {
@@ -108,4 +133,38 @@ function togglePlaying() {
     song.pause();
 
   }
+}
+
+// function spliceIntoChunks(arr, chunkSize) {
+//   const res = [];
+//   while (arr.length > 0) {
+//     const chunk = arr.splice(0, chunkSize);
+//     res.push(chunk);
+//   }
+//   return res;
+// }
+
+function splitIntoN(arr, num) {
+  const res = [];
+  const itemPerArray = Math.ceil(arr.length / num)
+  while (arr.length > 0) {
+    const chunk = arr.splice(0, itemPerArray);
+    res.push(chunk);
+  }
+  return res;
+}
+
+// function getAvg(arr) {
+//   let total = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     total += arr[i];
+//     //console.log(arr[i])
+
+//   }
+//   let avg = total / arr.length
+//   // console.log(avg)
+//   return avg;
+// }
+function calculateAvg(array) {
+  return array.reduce((a, b) => a + b) / array.length;
 }
